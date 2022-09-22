566a0df1dd01bd158899cda17557a0021cee51fd
Dmitrii Derendyaev <dds_stalker@mail.ru>
2022-09-22 19:31:59 +0300

    basic sample with worst algorythm

--- 21task/firstDay/ReallyPeimeNumber/src/PrimeNumber.java
+++ 21task/firstDay/ReallyPeimeNumber/src/PrimeNumber.java
@@ -3,11 +3,26 @@ import java.util.Scanner;
 public class PrimeNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int exitCode = 0;
+        int enteredNumber;
 
         System.out.println("Введите рассматриваемое число: ");
-        JustPrime researchNumber = new JustPrime(scanner.nextInt());
+        enteredNumber = scanner.nextInt();
 
-        System.out.println(researchNumber.isPrimeNumber() + " " + researchNumber.getCountOperation());
+        if(enteredNumber<=1){
+            exitCode = illegalArgument();
+        }
+        else {
+            JustPrime researchNumber = new JustPrime(enteredNumber);
+            System.out.println(researchNumber.isPrimeNumber() + " " + researchNumber.getCountOperation());
+        }
+        System.exit(exitCode);
+    }
 
+    public static int illegalArgument(){
+        System.err.println("Illegal Argument");
+        return(-1);
     }
 }
+
+
