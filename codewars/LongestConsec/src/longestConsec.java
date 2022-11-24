39ba06451ee4aea236651a4e6e33fe8340127761
Dmitrii Derendyaev <dds_stalker@mail.ru>
2022-11-24 21:37:41 +0300

    doesn't work

---
+++ codewars/LongestConsec/src/longestConsec.java
@@ -0,0 +1,11 @@
+class LongestConsec {
+
+    public static String longestConsec(String[] strarr, int k) {
+        String answer = "smth";
+
+        if(k > strarr.length || k <= 0 || strarr.length == 0)
+
+
+        return  answer;
+    }
+}
