b816b67ac08a905a0165f077057f0e427daf6499
Dmitrii Derendyaev <dds_stalker@mail.ru>
2022-11-29 01:02:53 +0300

    added role selector

---
+++ trash/RoleSelector/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        String [] roles= {
+                "Городничий","Аммос Федорович",
+                "Артемий Филиппович",
+                "Лука Лукич"};
+        String [] textLines={
+                "Городничий: Я пригласил вас, господа, с тем, чтобы сообщить вам пренеприятное известие: к нам едет ревизор.",
+                "Аммос Федорович: Как ревизор?",
+                "Артемий Филиппович: Как ревизор?",
+                "Городничий: Ревизор из Петербурга, инкогнито. И еще с секретным предписаньем.",
+                "Аммос Федорович: Вот те на!",
+                "Артемий Филиппович: Вот не было заботы, так подай!",
+                "Лука Лукич: Господи боже! еще и с секретным предписаньем!"};
+        StringBuilder answer = new StringBuilder();
+        for (String role : roles) {
+            answer.append(role + ":\n");
+            for (int i = 0; i < textLines.length; i++) {
+                if (textLines[i].startsWith(role + ":")) {
+                    answer.append((i+1) + ")" + textLines[i].substring(role.length() + 1) + "\n");
+                }
+            }
+            answer.append("\n");
+        }
+        System.out.println(answer.toString());
+    }
+}
