084940c3b7d2999e1f6140bef848ac45d0ad1fee
Dmitrii Derendyaev <dds_stalker@mail.ru>
2022-12-03 01:32:52 +0300

    remake import

--- trash/TelegramBot/dispatcher/src/main/java/org/example/controller/TelegramBotMe.java
+++ trash/TelegramBot/dispatcher/src/main/java/org/example/controller/TelegramBotMe.java
@@ -1,6 +1,5 @@
 package org.example.controller;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
