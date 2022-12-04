65bfc2f86e7d5e076415cc551c5d1f6accc16def
Dmitrii Derendyaev <dds_stalker@mail.ru>
2022-12-05 00:21:36 +0300

    added Logger class to save message in file

--- trash/TelegramBot/dispatcher/src/main/java/org/example/controller/TelegramBotMe.java
+++ trash/TelegramBot/dispatcher/src/main/java/org/example/controller/TelegramBotMe.java
@@ -1,11 +1,16 @@
 package org.example.controller;
 
+import lombok.extern.log4j.Log4j;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
+@Log4j
 public class TelegramBotMe extends TelegramLongPollingBot {
     @Value("${bot.name}")
     private String botName;
@@ -25,6 +30,22 @@ public class TelegramBotMe extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         var originalMessage = update.getMessage();
-        System.out.println(originalMessage.getText());
+//        System.out.println(originalMessage.getText());
+        log.debug(originalMessage.getText());
+
+        var response = new SendMessage();
+        response.setChatId(originalMessage.getChatId().toString());
+        response.setText("Hello, everyone!");
+        sendAnswerMessage(response);
+    }
+
+    public void sendAnswerMessage(SendMessage message){
+        if(message != null){
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                log.error(e);
+            }
+        }
     }
 }
