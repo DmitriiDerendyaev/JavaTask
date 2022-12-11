dd96bb7beab351507eef11c2ab521d69835a6e75
Dmitrii Derendyaev <dds_stalker@mail.ru>
2022-12-12 01:30:44 +0300

    added Sum of Array

---
+++ codewars/SumOf_1D_Array/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] num = {1, 2, 3, 4};
+        System.out.println(Arrays.toString(Solution.runningSum(num)));
+    }
+}
