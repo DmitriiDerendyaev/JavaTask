b762905a35b8b66f16355dd8eba64f2a3552552c
Dmitrii Derendyaev <dds_stalker@mail.ru>
2022-12-17 15:29:04 +0300

    added wrong solution, see prev commit

--- codewars/Ransom Note/src/Main.java
+++ codewars/Ransom Note/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Solution.canConstruct("aab", "baa"));
+        System.out.println(Solution.canConstruct("ab", "acd"));
     }
 }
--- codewars/Ransom Note/src/Solution.java
+++ codewars/Ransom Note/src/Solution.java
@@ -1,25 +1,56 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.ArrayList;
+
+//class Solution {
+//    public static boolean canConstruct(String ransomNote, String magazine) {
+//
+//        Map<Character, Integer> ransomCounts = new HashMap<>();
+//        Map<Character, Integer> magazineCounts = new HashMap<>();
+//
+//        for (char ch : ransomNote.toCharArray()) {
+//            ransomCounts.put(ch, ransomCounts.getOrDefault(ch, 0) + 1);
+//        }
+//
+//        for (char ch : magazine.toCharArray()) {
+//            magazineCounts.put(ch, magazineCounts.getOrDefault(ch, 0) + 1);
+//        }
+//
+//        for (char ch : ransomCounts.keySet()) {
+//            if (!magazineCounts.containsKey(ch) || magazineCounts.get(ch) < ransomCounts.get(ch)) {
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//}
 
 class Solution {
     public static boolean canConstruct(String ransomNote, String magazine) {
+        ArrayList<Character> ransomList = new ArrayList<>();
+        ArrayList<Character> magazineList = new ArrayList<>();
 
-        Map<Character, Integer> ransomCounts = new HashMap<>();
-        Map<Character, Integer> magazineCounts = new HashMap<>();
+        char[] ransomStrinigArray = ransomNote.toCharArray();
+        char[] magazineStrinigArray = magazine.toCharArray();
 
-        for (char ch : ransomNote.toCharArray()) {
-            ransomCounts.put(ch, ransomCounts.getOrDefault(ch, 0) + 1);
+        for (int i = 0; i < ransomNote.length(); i++){
+            ransomList.add(ransomStrinigArray[i]);
         }
 
-        for (char ch : magazine.toCharArray()) {
-            magazineCounts.put(ch, magazineCounts.getOrDefault(ch, 0) + 1);
+
+        for (int i = 0; i < magazine.length(); i++){
+            magazineList.add(magazineStrinigArray[i]);
         }
 
-        for (char ch : ransomCounts.keySet()) {
-            if (!magazineCounts.containsKey(ch) || magazineCounts.get(ch) < ransomCounts.get(ch)) {
-                return false;
+        for(int i = 0; i < ransomList.size(); i++){
+            for(int j = 0; j < magazineList.size(); j++){
+                if(ransomList.get(i) == magazineList.get(j)){
+                    magazineList.remove(j);
+                }
             }
         }
+
+
+
         return true;
     }
 }
