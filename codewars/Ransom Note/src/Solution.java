e5eda1f4f3a4202f96cf1ba0cc5390b4c51747a9
Dmitrii Derendyaev <dds_stalker@mail.ru>
2022-12-17 14:25:21 +0300

    WOW

--- codewars/Ransom Note/src/Solution.java
+++ codewars/Ransom Note/src/Solution.java
@@ -1,7 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public static boolean canConstruct(String ransomNote, String magazine) {
-            if(magazine.indexOf(ransomNote) == -1) return false;
-            else return true;
+
+        Map<Character, Integer> ransomCounts = new HashMap<>();
+        Map<Character, Integer> magazineCounts = new HashMap<>();
+
+        for (char ch : ransomNote.toCharArray()) {
+            ransomCounts.put(ch, ransomCounts.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : magazine.toCharArray()) {
+            magazineCounts.put(ch, magazineCounts.getOrDefault(ch, 0) + 1);
+        }
+
+        for (char ch : ransomCounts.keySet()) {
+            if (!magazineCounts.containsKey(ch) || magazineCounts.get(ch) < ransomCounts.get(ch)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
