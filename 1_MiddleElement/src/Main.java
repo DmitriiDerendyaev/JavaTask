967da41704291612064cea37828d9ab100602a08
Dmitrii Derendyaev <dds_stalker@mail.ru>
2025-08-28 23:47:29 +0400

    answer: 1. Средний элемент - improve solution

--- 1_MiddleElement/src/Main.java
+++ 1_MiddleElement/src/Main.java
@@ -3,25 +3,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        Пример ввода и вывода числа n, где -10^9 < n < 10^9:
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        System.out.println(n);
-        */
-
         Scanner scanner = new Scanner(System.in);
 
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-           list.add(scanner.nextInt());
-        }
-
-        Collections.sort(list);
-
+        List<Integer> list = IntStream.range(0, 3).mapToObj(i -> scanner.nextInt()).sorted().toList();
         System.out.println(list.get(1));
     }
 }
